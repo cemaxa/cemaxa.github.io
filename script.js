ae5d8e5c5ce8b1b7b016b42d810ae75c1fbb816a
cemaxa <celoveka097@gmail.com>
2026-05-19 21:48:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const btn = document.getElementById("portalBtn");
+const whispers = document.querySelectorAll(".whispers span");
 
-btn.addEventListener("click", ()=>{
+setInterval(()=>{
+
+    whispers.forEach(w=>{
+
+        const dx=(Math.random()*30)-15;
+        const dy=(Math.random()*30)-15;
+
+        w.style.transform=
+            `translate(${dx}px,${dy}px)`;
+
+    });
+
+},1200);
+
+btn.addEventListener("click",()=>{
 
     document.body.classList.add("horror");
 
-    btn.disabled = true;
-    btn.innerText = "смотри...";
+    btn.disabled=true;
+    btn.innerText="смотри...";
 
     setTimeout(()=>{
         window.location.href="https://t.me/+KOEmylJiUcoyNmRi";
